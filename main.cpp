a167b6d58217e781d9ed6592532c246950466126
Mauricio Zambrano <wichoz2000@gmail.com>
2020-11-17 10:38:06 -0600

    Outdegrees
    Print max outdegrees and outdegrees in range and find max outdegree.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,62 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
+
 
 using namespace std;
 
-int main(){
+// void load(){
 
+//     to_string(stoi(mapaIp[i][1])++);
+// }
+
+int outdegree(vector< vector< int> > &listAdj, unordered_map<int, vector<string> > &mapaIp, int n){
+    int max = 0;
+
+    for(int i = 0; i < n; i++){
+        if(stoi(mapaIp[i][1]) > max){   //Converts outdegree string to int and compares with max
+            max = stoi(mapaIp[i][1]);
+        }
+    }
+
+    return max;
+}
+
+//Imprime todos los vectores con outdegree maximo
+void printIpMaximumOutdegree(int max, unordered_map<int, vector<string> > &mapaIp, int n){
+    cout << "Ip's con mayor numero de fanouts:" << endl;
+    cout << "Fanouts: " << max << endl;
+
+    for(int i = 0; i < n; i++){
+        if(stoi(mapaIp[i][1]) == max){
+            cout << mapaIp[i][0] << endl;
+        }
+    }
+}
+
+void printIpRangoOutdegree(unordered_map<int, vector<string> > &mapaIp, int n){
+    cout << "Ingrese número mínimo de fanouts que quisiera consultar:    (El número debería de ser mayor a x)" << endl;  //PONER NUMERO MENOR POSIBLE
+    
+    int max;
+
+    cin >> max;
+
+    cout << "Fanouts: " << max << endl;
+
+    for(int i = 0; i < n; i++){
+        if(stoi(mapaIp[i][1]) >= max){
+            cout << mapaIp[i][0] << " tiene " << mapaIp[i][1] << " fanouts." << endl;
+        }
+    }
+}
+
+int main(){
+    
+
+
+    //A la hora de leer meter a mapa [key, [ip, numero de outdegree(en string) ] ]
+    unordered_map<int, vector<string> > mapaIp;
 
     return 0;
 }
