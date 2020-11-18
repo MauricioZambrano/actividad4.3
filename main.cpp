b655f0b759cc8d7ee613be6edd8ca737ebc7e76f
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2020-11-18 16:41:53 -0600

    Documentación
    Nombres y descripción de la actividad

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-// ACTIVIDAD 4.3
-// Mauricio E. Zambrano - A00827055
-// Fernando Doddoli - A00827038
-// Cristobal Escamilla - A008
+/*
+ * Programación de estructuras de datos y algoritmos fundamentales
+ * Actividad 4.3 - Actividad integral de grafos
+ * 
+ * Equipo 13
+ * 
+ * Fernando Doddoli Lankenau - A00827038
+ * Mauricio Eugenio Zambrano Díaz - A00827055
+ * Cristóbal Alberto Escamilla Sada - A00827074
+ * 
+ * 20 de noviembre del 2020
+ */
 
 
 
